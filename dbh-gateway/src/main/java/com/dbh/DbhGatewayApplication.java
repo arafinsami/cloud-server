c3394d36e5af2a9e0c4cd817ac5b59a27b238cc4
MD. SAMIUL ARAFIN <sami.cse.1112@gmail.com>
2024-07-14 01:27:27 +0900

    gateway config added

--- dbh-gateway/src/main/java/com/dbh/DbhGatewayApplication.java
+++ dbh-gateway/src/main/java/com/dbh/DbhGatewayApplication.java
@@ -3,29 +3,11 @@ package com.dbh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class DbhGatewayApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(DbhGatewayApplication.class, args);
-	}
-
-	@Bean
-	public CorsWebFilter corsWebFilter() {
-		CorsConfiguration corsConfig = new CorsConfiguration();
-		corsConfig.setAllowedOrigins(Arrays.asList("*"));
-		corsConfig.setMaxAge(8000L);
-		corsConfig.addAllowedMethod("*");
-		corsConfig.addAllowedHeader("*");
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", corsConfig);
-		return new CorsWebFilter(source);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DbhGatewayApplication.class, args);
+    }
 }
